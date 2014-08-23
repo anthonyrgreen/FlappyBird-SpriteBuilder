a9a1d794d50e1864911111ed38e8d41c9aff36a0
danielrgreen <drgreen@fas.harvard.edu>
2014-08-23 17:34:17 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -8,7 +8,7 @@
 {
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
-    [self addObstacle]
+    [self addObstacle];
 }
 
 -(void)update:(CCTime)delta
